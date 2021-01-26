ae42de1778928a71b477ec3899d6706e0dfc049a
RemnantStudios <daro.peeters@hotmail.com>
2021-01-26 10:26:05 +0100

    Hello World - Multiplayer Edition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 movement = new Vector3(horizontal, vertical, 0f);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
